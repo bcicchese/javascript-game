e055b4c24125883219d0bcfaa6452ab091e15bab
bcicchese <b.cicchese@gmail.com>
2018-08-11 11:56:19 -0600

    working on finishing touches. added modal functionality

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ var deck = document.querySelector('.deck');
 var stars = [...document.getElementsByClassName('fa-star')];
 let moves = document.querySelector('.moves');
 let restart = document.querySelector('.restart');
+let replay = document.getElementById('replayBtn');
 let counter = 0;
 let time = 0;
 let timer;
@@ -18,7 +19,7 @@ let openCards = [];
 *   - loop through each card and create its HTML
 *   - add each card's HTML to the page
 */
-document.onload = startGame()
+window.onload = startGame()
 
 function startGame() {
   var shuffledDeck = shuffle(cardDeck);
@@ -49,7 +50,7 @@ function shuffle(array) {
 function changeClass(e){
   e.classList.toggle('open');
   e.classList.toggle('show');
-  }
+}
 
 function addToOpen(e){
   openCards.push(e);
@@ -91,7 +92,7 @@ function shake(e) {
 }
 
 function spin(e) {
-  //all cards spin upon winning
+  e.classList.add('spin');
 }
 
 function changeMoves(){
@@ -99,32 +100,35 @@ function changeMoves(){
   moves.innerHTML = counter;
   switch(true){
     case (counter === 9):
-      if(stars[0].classList.contains('fa-star')) {
-          stars[0].classList.add('fa-star-o')
-      };
+    if(stars[0].classList.contains('fa-star')) {
+      stars[0].classList.add('fa-star-o')
+    };
     break;
     case (counter === 15):
     if(stars[1].classList.contains('fa-star')) {
-        stars[1].classList.add('fa-star-o')
+      stars[1].classList.add('fa-star-o')
     };
     break;
     case (counter >= 20):
     if(stars[0].classList.contains('fa-star')) {
-        stars[0].classList.add('fa-star-o')
+      stars[0].classList.add('fa-star-o')
     };
-      break;
+    break;
   }
 }
 
 function endGame(){
   if(matchedCards === 8) {
-    modal.style.display = "block";
-    stopTimer();
+    setTimeout(() => {
+      modal.style.display = "block";
+    }, 2000);
+    clearInterval(timer);
+    cardDeck.forEach(spin);
+    inputStats();
   }
 }
 
 //timer
-
 function startTimer() {
   timer = setInterval(function(){
     time++;
@@ -143,11 +147,13 @@ function startTimer() {
   }, 10);
 }
 
-function stopTimer() {
-  clearInterval(timer);
-}
+restart.addEventListener('click', function() {
+  location.reload();
+})
 
-restart.addEventListener('click', startGame);
+replay.addEventListener('click', function() {
+  location.reload();
+})
 
 deck.addEventListener('click', function(event){
   const e = event.target;
@@ -167,26 +173,34 @@ var modal = document.getElementById('myModal');
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+function inputStats() {
+  const stoppedTimer = document.getElementById('timer').innerHTML;
+  const stars = document.querySelector('.stars').innerHTML;
+  document.querySelector('.modal-moves').innerHTML = `Moves: ${counter}`;
+  document.querySelector('.modal-time').innerHTML = `Time: ${stoppedTimer}`;
+  document.querySelector('.modal-stars').innerHTML = `Stars: ${stars}`;
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+  // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
-/*
-*General Instructions
-* set up the event listener for a card. If a card is clicked:
-*  - display the card's symbol (put this functionality in another function that you call from this one)
-*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-*  - if the list already has another card, check to see if the two cards match
-*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-*/
+  /*
+  *General Instructions
+  * set up the event listener for a card. If a card is clicked:
+  *  - display the card's symbol (put this functionality in another function that you call from this one)
+  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+  *  - if the list already has another card, check to see if the two cards match
+  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+  */
